cfe4aeb13ecd1cf677cf33d6a4018c53fd5fe738
vitruvianarendt <hanahh2001@gmail.com>
2023-04-18 00:25:14 +0200

    Added Hello World component and service, made a call to the Hello World endpoint and displayed the received response

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hi', loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
