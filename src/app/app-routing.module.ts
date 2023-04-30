5566c56e790a208ee73a5eb29ee0e13763c0a15f
milkovskamarija <m.milkovska12@gmail.com>
2023-04-30 22:59:01 +0200

    Added the homepage for FurryFeet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: 'hi', loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule) }
+  {
+    path: 'hi',
+    loadChildren: () =>
+      import('./hello/hello.module').then((m) => m.HelloModule),
+  },
+  { path: 'home', component: HomePageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
