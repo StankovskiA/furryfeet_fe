4935958a4dd8ceb63656a6b719474292e6e1da25
StankovskiA <stankovskia@gmail.com>
2023-05-27 17:10:50 +0200

    UIK-28: Add Random Facts, Improve coloring

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+const dogFacts: string[] = [
+  "Dogs have an exceptional sense of smell, about 1,000 times more powerful than humans.",
+  "A dog's nose print is unique, much like a person's fingerprint.",
+  "Dalmatians are born completely white and develop their spots as they grow.",
+  "Dogs have three eyelids: an upper lid, a lower lid, and a third lid called a haw that helps protect their eyes.",
+  "The world's smallest dog breed is the Chihuahua, while the largest is the Great Dane.",
+  "Dogs have a specialized region in their brains for processing human faces.",
+  "A dog's sense of hearing is four times more sensitive than a human's.",
+  "Some dogs can learn over 1,000 words and commands.",
+  "Dogs sweat through their paws rather than their bodies like humans.",
+  "The average dog can run at speeds of up to 19 miles (30 kilometers) per hour."
+];
+
+function getRandomFact(exclude: string): string {
+  let randomIndex: number;
+  let fact: string;
+
+  do {
+    randomIndex = Math.floor(Math.random() * dogFacts.length);
+    fact = dogFacts[randomIndex];
+  } while (fact === exclude);
+
+  return fact;
+}
 
 @Component({
   selector: 'app-about',
@@ -6,5 +31,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  cardText1: string = getRandomFact('');
+  cardText2: string = getRandomFact(this.cardText1);
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
       { path: 'changepassword', component: ChangePasswordComponent },
     ], 
   },
+  { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutComponent }
 
